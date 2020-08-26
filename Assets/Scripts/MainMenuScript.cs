e9c5bf1484273595706613f0482d5263245a7528
fabrice-noel29 <fabrice.noel29@gmail.com>
2020-08-26 16:25:16 +0200

    Corrected Level Design
    Corrected Level Design

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -8,15 +8,14 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public GameObject newPlayer;
+    
     public GameObject mainMenu;
 
     public void NewGame()
     {
-        newPlayer.SetActive(true);
         mainMenu.SetActive(false);
-        //PlayerPrefs.SetInt("DernierNiveau", 0);
-        //sceneManager.LoadScene(0);
+        PlayerPrefs.SetInt("DernierNiveau", 0);
+        SceneManager.LoadScene(1);
     }
 
     public void LoadGame()
